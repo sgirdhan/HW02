96ae898a21999fdf4cf736ac07a0ad82e7f5b302
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-09-14 22:20:12 -0400

    Initial commit

--- app/src/main/java/com/example/sharangirdhani/homework02/MainActivity.java
+++ app/src/main/java/com/example/sharangirdhani/homework02/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.sharangirdhani.homework02;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.sharangirdhani.homework02.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
