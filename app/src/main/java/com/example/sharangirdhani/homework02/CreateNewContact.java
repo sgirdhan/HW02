c28cea4c8a654609620beba7589559ee682d3484
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-09-15 16:07:14 -0400

    Create Contact

--- app/src/main/java/com/example/sharangirdhani/homework02/CreateNewContact.java
+++ app/src/main/java/com/example/sharangirdhani/homework02/CreateNewContact.java
@@ -1,29 +1,48 @@
 package com.example.sharangirdhani.homework02;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.sharangirdhani.homework02.databinding.ActivityMainBinding;
+
 
 public class CreateNewContact extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1 ;
+    private ActivityMainBinding binding;
+    ImageButton img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_contact);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_new_contact);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_create_new_contact);
+
     }
 
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            img = (ImageButton) findViewById(R.id.imageButton);
+            img.setImageBitmap(imageBitmap);
+        }
+    }
 }
--- app/src/main/java/com/example/sharangirdhani/homework02/MainActivity.java
+++ app/src/main/java/com/example/sharangirdhani/homework02/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sharangirdhani.homework02;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sharangirdhani.homework02.databinding.ActivityMainBinding;
 
@@ -14,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    public void createNew(View view) {
+        Intent intent = new Intent(MainActivity.this, CreateNewContact.class);
+        startActivity(intent);
+
     }
 }
